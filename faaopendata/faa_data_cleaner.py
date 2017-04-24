51ff9876ee4d8856f882834db8f8b730c979ba3c
gisdev01 <gisdev01@gmail.com>
2017-04-24 18:51:35 -0400

    Update FAA DB data import to deal with dirty data

--- faaopendata/faa_data_cleaner.py
+++ faaopendata/faa_data_cleaner.py
@@ -5,7 +5,7 @@ import csv
 # MASTER.txt is from https://www.faa.gov/licenses_certificates/aircraft_certification/aircraft_registry/releasable_aircraft_download/
 with open("MASTER.txt") as orig_file:
     orig_file_reader = csv.reader(orig_file, delimiter=",")
-    with open("MASTER_CLEANED.csv", "w", newline='') as clean_file:
+    with open("/temp/MASTER_CLEANED.csv", "w", newline='') as clean_file:
         writer = csv.writer(clean_file)
         for orig_record in orig_file_reader:
             new_row = [old_field_data.strip() for old_field_data in orig_record]
