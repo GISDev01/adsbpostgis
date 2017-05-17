e7e0b8b723382e7a187e02c6a3052dacefc84bbe
gisdev01 <gisdev01@gmail.com>
2017-05-16 20:28:19 -0400

    Add comments to explain what the script is doing on ingest

--- faaopendata/faa_data_cleaner.py
+++ faaopendata/faa_data_cleaner.py
@@ -8,7 +8,11 @@ with open("MASTER.txt") as orig_file:
     with open("/temp/MASTER_CLEANED.csv", "w", newline='') as clean_file:
         writer = csv.writer(clean_file)
         for orig_record in orig_file_reader:
+            # some of the data contains trailing spaces/tabs, so we remove those first
             new_row = [old_field_data.strip() for old_field_data in orig_record]
+
             # The data has a trailing comma on every single row (including the header row), so remove it as well
             new_row_without_trailing_comma = new_row[:-1]
+
+            # write out a new CSV, which is then imported into Postgres
             writer.writerow(new_row_without_trailing_comma)
