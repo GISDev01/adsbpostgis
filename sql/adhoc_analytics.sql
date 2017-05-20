b56332128b75422f07c2c616715f7f6e76235555
gisdev01 <gisdev01@gmail.com>
2017-05-20 09:22:35 -0400

    New query to check for ano-n mode-s-hex results

--- sql/adhoc_analytics.sql
+++ sql/adhoc_analytics.sql
@@ -1,3 +1,5 @@
 SELECT COUNT(*) FROM aircraftreports;
 SELECT COUNT(*) FROM aircraftreports WHERE altitude < 1000;
 SELECT COUNT(*) FROM aircraftreports WHERE altitude > 40000;
+SELECT COUNT(*) FROM aircraftreports WHERE SUBSTR(mode_s_hex, 1, 1) = '~';
+
