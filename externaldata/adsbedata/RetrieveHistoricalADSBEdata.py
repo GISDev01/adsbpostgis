a196a60a497f724f7ea087ae88629d0a037be953
gisdev01 <gisdev01@gmail.com>
2017-05-30 22:10:34 -0400

    Begin adding new functionality for extracting relevant data from historical archive ADSBE downloads

--- externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
+++ externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
@@ -1,7 +1,17 @@
 import requests
+import io
+import zipfile
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-# http://history.adsbexchange.com/Aircraftlist.json/
-# http://history.adsbexchange.com/Aircraftlist.json/2016-06-20.zip
+def get_archive_zip(zip_url):
+    req = requests.get(zip_url)
+    res_zip = zipfile.ZipFile(io.BytesIO(req.content))
+
+    return res_zip.extractall()
 
 
+get_archive_zip('http://history.adsbexchange.com/Aircraftlist.json/2016-06-20.zip')
