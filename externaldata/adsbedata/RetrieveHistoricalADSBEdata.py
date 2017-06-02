37acc5a57acb06777fd567aeab516406714231d1
gisdev01 <gisdev01@gmail.com>
2017-06-02 16:19:40 -0400

    Historical data download - continue functionality update

--- externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
+++ externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
@@ -1,6 +1,7 @@
 import requests
 import io
 import zipfile
+import os
 
 import logging
 
@@ -10,8 +11,9 @@ logger = logging.getLogger(__name__)
 def get_archive_zip(zip_url):
     req = requests.get(zip_url)
     res_zip = zipfile.ZipFile(io.BytesIO(req.content))
-
-    return res_zip.extractall()
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    return res_zip.extractall('output')
 
 
 get_archive_zip('http://history.adsbexchange.com/Aircraftlist.json/2016-06-20.zip')
