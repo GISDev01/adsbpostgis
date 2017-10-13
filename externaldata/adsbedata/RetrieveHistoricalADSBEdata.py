8996d51b2fcdc339d19de18d5ba21b184994f550
gisdev01 <gisdev01@gmail.com>
2017-10-12 20:08:41 -0400

    Add range of timestamps to retrieve appropriate files to ingest within Area of Interestπ

--- externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
+++ externaldata/adsbedata/RetrieveHistoricalADSBEdata.py
@@ -7,8 +7,7 @@ import requests
 import io
 import zipfile
 import os
-from time import strftime
-
+import datetime
 import logging
 import yaml
 from model import aircraft_report
@@ -19,7 +18,7 @@ zip_dir = 'output'
 # TODO: dynamically calculate range of dates
 # current_date_stamp = strftime('%y-%m-%d')
 current_date_stamp = '2017-10-01'
-
+datestamp_format = '%Y-%m-%d'
 parent_dir = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -38,9 +37,20 @@ def get_and_load_archive_data_by_date(zip_url):
     aircraft_report.get_aircraft_data_from_files(os.path.join(parent_dir, zip_dir))
 
 
+def get_list_of_datestamps_inclusive(start_date, end_date):
+    datestamps_list = []
+    start_datestamp = datetime.datetime.strptime(start_date, datestamp_format)
+    end_datestamp = datetime.datetime.strptime(end_date, datestamp_format)
+    step_size = datetime.timedelta(days=1)
+    while start_datestamp <= end_datestamp:
+        datestamps_list += start_datestamp
+        start_datestamp += step_size
+
+    return datestamps_list
+
+
 local_config = get_config()
 adsbe_download_base_url = local_config['adsbe_url']
-
 
 dl_url = adsbe_download_base_url + '{}.zip'.format(current_date_stamp)
 print(dl_url)
