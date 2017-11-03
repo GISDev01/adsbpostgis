26458f79fa6ebe13a6509ccdff914df3391d7c5f
gisdev01 <gisdev01@gmail.com>
2017-11-02 21:42:50 -0400

    Set up for a quick iterator test to detect specific spatiotemporal patterns in the data. Will re-implement in Spark with Esri geometry API to run it on the 25B+ point dataset after I get it working on this small test caseˆ

---
+++ analysis/AdHocAnalytics.py
@@ -0,0 +1,35 @@
+from utils import postgres as pg_utils
+import os
+import yaml
+import logging
+
+with open('../config.yml', 'r') as yaml_config_file:
+    config = yaml.load(yaml_config_file)
+
+# log_formatter = logging.Formatter("%(levelname)s: %(asctime)s - %(name)s - %(process)s - %(message)s")
+FORMAT = '%(asctime)-15s %(levelname)s: %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
+
+db_hostname = config['database']['hostname']
+db_port = config['database']['port']
+db_name = config['database']['dbname']
+db_user = config['database']['user']
+db_pwd = config['database']['pwd']
+
+dbconn = pg_utils.database_connection(dbname=db_name,
+                                      dbhost=db_hostname,
+                                      dbport=db_port,
+                                      dbuser=db_user,
+                                      dbpasswd=db_pwd)
+
+cur = dbconn.cursor()
+sql = '''
+		SELECT *
+			FROM aircraftreports 
+			  WHERE mode_s_hex like \'%s\' ''' % 'modes'
+
+cur.execute(sql)
+for row in cur:
+    logger.info(row)
+
