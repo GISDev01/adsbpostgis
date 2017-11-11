249e844e7d788788613e4425f39565430e7a90cd
gisdev01 <gisdev01@gmail.com>
2017-11-11 17:35:46 -0500

    Update patterns table with new indexesπ

--- sql/postgres_pattern_setup.sql
+++ sql/postgres_pattern_setup.sql
@@ -1,13 +1,26 @@
 CREATE TABLE aircraftpatterns (
-  mode_s_hex      TEXT,
-  report_location GEOGRAPHY(Point, 4326),
-  longitude83     DOUBLE PRECISION,
-  latitude83      DOUBLE PRECISION,
-  report_epoch    INTEGER,
-  reporter        TEXT,
-  itinerary_id    TEXT
+  mode_s_hex              TEXT,
+  pattern_centroid        GEOGRAPHY(Point, 4326),
+  pattern_cent_long       DOUBLE PRECISION,
+  pattern_cent_lat        DOUBLE PRECISION,
+  report_epoch            INTEGER,
+  reporter                TEXT,
+  itinerary_id            TEXT
 );
 
 
 ALTER TABLE aircraftpatterns
   OWNER TO postgres;
+
+
+CREATE INDEX mode_s_hex_idx
+  ON aircraftpatterns USING BTREE (mode_s_hex);
+
+CREATE INDEX pr_epoch
+  ON aircraftpatterns USING BTREE (report_epoch);
+
+CREATE INDEX pat_cent_loc
+  ON aircraftpatterns USING GIST (pattern_centroid);
+
+GRANT ALL ON TABLE aircraftpatterns TO postgres;
+
