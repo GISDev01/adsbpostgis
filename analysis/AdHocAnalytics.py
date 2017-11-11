82afaa4be3412fbcb5376c3b661c7bdd062894bc
gisdev01 <gisdev01@gmail.com>
2017-11-10 20:32:45 -0500

    Refactor Itinerary Assignment and begin testing of pattern detection automationπ

--- analysis/AdHocAnalytics.py
+++ analysis/AdHocAnalytics.py
@@ -19,7 +19,6 @@ db_name = config['database']['dbname']
 db_user = config['database']['user']
 db_pwd = config['database']['pwd']
 
-ITINERARY_MAX_TIME_DIFF_SECONDS = int(config['itinerarymaxtimediffseconds'])
 
 dbconn = pg_utils.database_connection(dbname=db_name,
                                       dbhost=db_hostname,
@@ -28,18 +27,7 @@ dbconn = pg_utils.database_connection(dbname=db_name,
                                       dbpasswd=db_pwd)
 
 
-def get_all_unique_mode_s_without_itin_assigned():
-    logger.info('Fetching a list of all Mode S Idents missing itin ID.')
-    uniq_mode_s_cursor = dbconn.cursor()
-
-    sql = '''SELECT DISTINCT aircraftreports.mode_s_hex FROM aircraftreports 
-              WHERE aircraftreports.itinerary_id IS NULL'''
-    uniq_mode_s_cursor.execute(sql)
-
-    return [record[0] for record in uniq_mode_s_cursor.fetchall()]
-
-
-def assign_itinerary_id_for_mode_s(mode_s_hex_for_update, itinerary_id, min_time, max_time):
+def placeholder(mode_s_hex_for_update, itinerary_id, min_time, max_time):
     min_timestamp = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(min_time))
     max_timestamp = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(max_time))
 
@@ -60,59 +48,12 @@ def assign_itinerary_id_for_mode_s(mode_s_hex_for_update, itinerary_id, min_time
     #                                                                                         max_time))
 
 
-def calc_time_diffs_for_mode_s(mode_s_hex):
-    """
-    Given an input of a string mode_s_hex code, query the DB for all records with that mode_s_hex and loop through
-    the record in order of timestamp, comparing each pair of records to determine the amount of time between
-    each point. If the 2 points are far arapt, it is assumed that the aircraft landed and took back off.
-    Note: this doesn't assign an ID for all records (the most recent batch), because the DB could be in the
-    middle of an itinerary when this script is run.
-    :param mode_s_hex: the hex code identifying the aircraft
-    :type mode_s_hex: str
-    :return: None
-    """
-    logger.info('Calcing Time Diffs to assign itinerary ids for mode s: {}'.format(mode_s_hex))
-    uniq_mode_s_cursor = dbconn.cursor()
+def find_patterns_within_itinerary(mode_s_hex):
+    pattern_cursor = dbconn.cursor()
 
-    sql = '''SELECT aircraftreports.report_epoch, aircraftreports.report_epoch - lag(aircraftreports.report_epoch)
-                OVER (ORDER BY aircraftreports.report_epoch) 
-                  AS time_delta_sec
-             FROM aircraftreports 
-              WHERE aircraftreports.itinerary_id IS NULL AND aircraftreports.mode_s_hex = '{}'
-                    ORDER BY aircraftreports.report_epoch'''.format(mode_s_hex)
+    sql = '''SELECT * FROM find_pattern_num('A8D33A');'''.format(mode_s_hex)
 
-    uniq_mode_s_cursor.execute(sql)
+    pattern_cursor.execute(sql)
 
-    count = 0
-    for time_diff_tuple in uniq_mode_s_cursor.fetchall():
-        logger.info('Time Diff: {}'.format(time_diff_tuple))
-        curr_timestamp = time_diff_tuple[0]
-
-        # Time difference between the current record and the previous record
-        time_diff_sec = time_diff_tuple[1]
-
-        if count == 0:
-            minimum_timestamp = time_diff_tuple[0]
-            count += 1
-            continue
-
-        if time_diff_sec > ITINERARY_MAX_TIME_DIFF_SECONDS:
-            maximum_timestamp = curr_timestamp
-            assign_itinerary_id_for_mode_s(itinerary_id=generate_itinerary_id(mode_s_hex, minimum_timestamp),
-                                           mode_s_hex_for_update=mode_s_hex,
-                                           min_time=minimum_timestamp,
-                                           max_time=maximum_timestamp)
-            count = 0
-        else:
-            count += 1
-
-
-def generate_itinerary_id(mode_s, epoch_timestamp):
-    timestamp = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(epoch_timestamp))
-    itineraryid = timestamp + '_{}'.format(mode_s)
-    logger.info('Itinerary ID Generated: {}'.format(itineraryid))
-    return itineraryid
-
-
-# get_unique_mode_s_without_itin_assigned()
-calc_time_diffs_for_mode_s('ADAFB5')
+    for record in pattern_cursor.fetchall():
+        logger.info('Pattern Result: {}'.format(record))
